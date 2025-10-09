031aa7dcf38f48e6d89a0eb86f5b2d23390b41ca
Ajay9084 <ajaypatidar9084@gmail.com>
2025-10-09 22:09:06 +0530

    Background Jobs - Inngest Server

---
+++ src/inngest/functions.ts
@@ -0,0 +1,16 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+
+    //Imagine this is a download step
+    await step.sleep("wait-a-moment", "1s");
+
+   
+
+
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
